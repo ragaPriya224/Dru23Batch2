ddca6f3049c3f97023066bd2a87c565d6573b638
priya <priya@zawqwe.com>
2023-07-28 13:02:07 +0530

    flatmap and optional

--- java/JavaDemoDruB2/src/practice/java8Demo/_4filter_transform/FlatMapDemo.java
+++ java/JavaDemoDruB2/src/practice/java8Demo/_4filter_transform/FlatMapDemo.java
@@ -3,23 +3,42 @@ package practice.java8Demo._4filter_transform;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FlatMapDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FlatMapDemo d = new FlatMapDemo();
+		d.m1();
 	}
 	public void m1() {
 		List<List<String>> nameList = new ArrayList<>();
 		List<String> abc = Arrays.asList("a","z","b");
 		List<String> def = Arrays.asList("y","ch","cd");
 		List<String> ghi = Arrays.asList("zl","ab","qwe");
+		nameList.add(abc);
 		nameList.add(def);
-		nameList.add(def);
-		nameList.add(def);
+		nameList.add(ghi);
+		System.out.println(nameList);
 
-
+		//without flatmap
+		List<String> namesDataList = new ArrayList<>();
+		for(List<String> listOfNames:nameList) {
+			for(String name:listOfNames) {
+				namesDataList.add(name);
+			}
+		}
+		System.out.println(namesDataList);
+		
+		
+		// with flatmap 
+		List<String> outputList = nameList.stream()
+		.flatMap(List::stream)
+//		.flatMap(d -> d.stream())
+		.collect(Collectors.toList());
+		System.out.println(outputList);
+		
 	}
 
 }
---
+++ java/JavaDemoDruB2/src/practice/java8Demo/optional/OptionalDemo.java
@@ -0,0 +1,28 @@
+package practice.java8Demo.optional;
+
+import java.util.Optional;
+
+public class OptionalDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String name = null;
+//		name.toLowerCase();
+		
+		String[] name = new String[10];
+		name[2] = "hello";
+		Optional<String> checkNull = Optional.ofNullable(name[2]);
+		System.out.println(checkNull);
+//		System.out.println(checkNull.get());
+//		if(checkNull.isPresent()) {
+//			String word = name[2].toLowerCase();
+//			System.out.println(word);
+//		}else {
+//			System.out.println("else block");
+//		}
+		if(checkNull.isEmpty()) {
+			System.out.println("value not there");
+		}
+	}
+
+}
