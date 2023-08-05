7853efe027fa8208b96c8180b30be2bf2577ef36
priya <priya@zawqwe.com>
2023-08-05 16:20:37 +0530

    get method with string, list of string return types

--- springboot/springAlphaDemo/src/main/java/com/example/demo/HelloController.java
+++ springboot/springAlphaDemo/src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,24 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+	
+//	@RequestMapping("/hello")
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hai user";
+	}
+	@GetMapping("/name")
+	public List<String> getNameList() {
+		List<String> nameList = Arrays.asList("aa","bb","cc");
+		return nameList;
+	}
+//	@RequestMapping(method = RequestMethod.POST)
 }
