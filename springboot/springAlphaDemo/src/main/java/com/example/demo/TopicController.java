4131443a4e2afc080ebaf54ee69e7fbce9eec229
priya <priya@zawqwe.com>
2023-08-07 17:28:14 +0530

    crud with pagination

--- springboot/springAlphaDemo/src/main/java/com/example/demo/TopicController.java
+++ springboot/springAlphaDemo/src/main/java/com/example/demo/TopicController.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -38,5 +41,11 @@ public class TopicController {
 	public void updateTopic(@RequestBody Topic topic,
 			@PathVariable String topicId) {
 		topicService.updateTopic(topic,topicId);
+	}
+	//localhost:8080/topic/page?pageNo=0&sort=subTopic
+	@GetMapping("/topic/page")
+	public Page<Topic> getTopicsByPage(@RequestParam("pageNo") Optional<Integer> pageParam,
+			@RequestParam("sort") Optional<String> sortBy){
+		return topicService.getTopicByPage(pageParam,sortBy);
 	}
 }
--- springboot/springAlphaDemo/src/main/java/com/example/demo/TopicService.java
+++ springboot/springAlphaDemo/src/main/java/com/example/demo/TopicService.java
@@ -1,47 +1,64 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	
+
 	@Autowired
 	TopicRepository topicRepository;
-//	List<Topic> topicList  = new ArrayList<>(Arrays.asList(
-//			new Topic("11","basic english",4),
-//			new Topic("14","basic japanese",5),
-//			new Topic("100","basic telugu",1)
-//			));
-	
+	//	List<Topic> topicList  = new ArrayList<>(Arrays.asList(
+	//			new Topic("11","basic english",4),
+	//			new Topic("14","basic japanese",5),
+	//			new Topic("100","basic telugu",1)
+	//			));
+
 	public Topic getTopic(String topicId) {
-//		return topicList.stream().filter(t -> t.getId().equals(topicId))
-//		.findFirst().get();
+		//		return topicList.stream().filter(t -> t.getId().equals(topicId))
+		//		.findFirst().get();
 		return topicRepository.findById(topicId).get();
 	}
-	
+
 	public List<Topic> getAllTopic() {
-		return null;
-//		topicRepository.findAll().
-		
-//		return topicList;
+		List<Topic> topicsList = new ArrayList<>();
+		topicRepository.findAll().forEach(topicsList::add);
+		return topicsList;
+		//		topicRepository.findAll().
+
+		//		return topicList;
 	}
 
 	public void deleteTopic(String id) {
-//		topicList.removeIf(t -> t.getId().equals(id));
+		//		topicList.removeIf(t -> t.getId().equals(id));
 		topicRepository.deleteById(id);
 	}
 
 	public void addTopic(Topic topic) {
-//		topicList.add(topic);
+		//		topicList.add(topic);
 		topicRepository.save(topic);
 	}
 
 	public void updateTopic(Topic topic, String topicId) {
 		//
 		topicRepository.save(topic);
+	}
+
+	public Page<Topic> getTopicByPage(Optional<Integer> pageParam, Optional<String> sortBy) {
+		// TODO Auto-generated method stub
+
+		return 	topicRepository.findAll(PageRequest.of(
+				pageParam.orElse(0),//page number
+				3, //number of records per page
+				Direction.DESC,//desc
+				sortBy.orElse("id")));//based on column 
 	}
 
 
