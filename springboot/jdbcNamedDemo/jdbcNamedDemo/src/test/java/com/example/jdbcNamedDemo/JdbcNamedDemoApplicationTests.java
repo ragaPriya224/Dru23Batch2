9abef113c285775ac17c3ef5d33afdf26126949e
priya <priya@zawqwe.com>
2023-08-10 11:02:46 +0530

    initial commit

---
+++ springboot/jdbcNamedDemo/jdbcNamedDemo/src/test/java/com/example/jdbcNamedDemo/JdbcNamedDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcNamedDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcNamedDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
