7a4bd4074448a53e10513a685a61b8ed9b7069d7
priya <priya@zawqwe.com>
2023-08-14 09:24:54 +0530

    z #ms use bean to create rest template instance

--- springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
+++ springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,15 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MovieCatalogController {
+	
+	@Autowired
+	RestTemplate rt;
 
 	@GetMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
 		//1 -> get all rated movie id's
 		List<Rating> ratings = Arrays.asList(new Rating("222",4),
 				new Rating("41",5));
-		RestTemplate rt = new RestTemplate();
 		return ratings.stream().map(rating ->{
 			Movie movie =rt.getForObject("https://localhost:8082/movie/"+rating.getMoveiId(), Movie.class);
 			return new CatalogItem(movie.getName(),"test",rating.getRating());
--- springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogueServiceApplication.java
+++ springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogueServiceApplication.java
@@ -2,6 +2,8 @@ package com.practice.simple.MovieCatalogueService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogueServiceApplication {
@@ -9,5 +11,9 @@ public class MovieCatalogueServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
 	}
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 
+	}
 }
