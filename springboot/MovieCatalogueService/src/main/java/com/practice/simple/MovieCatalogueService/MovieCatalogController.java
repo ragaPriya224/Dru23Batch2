04742f5d662d160a893c9bdbcd3565cf2a6ff6a0
priya <priya@zawqwe.com>
2023-08-14 09:16:24 +0530

    z #ms hardcode datas for ratings and catalogue

--- springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
+++ springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
@@ -1,7 +1,8 @@
 package com.practice.simple.MovieCatalogueService;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +13,13 @@ public class MovieCatalogController {
 
 	@GetMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
-		return Collections.singletonList(
-				new CatalogItem("spiderman","saving galaxt",4));
+		//1 -> get all rated movie id's
+		List<Rating> ratings = Arrays.asList(new Rating("222",4),
+				new Rating("41",5));
+
+		return ratings.stream().map(rating ->{
+			return new CatalogItem("Transformer","test",4);
+		}).collect(Collectors.toList());
 	}
 }
 
