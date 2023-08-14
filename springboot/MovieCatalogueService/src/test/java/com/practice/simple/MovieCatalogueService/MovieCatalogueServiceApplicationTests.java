4a5df30d27614ba8e44373d5a6a21717f0315bb6
priya <priya@zawqwe.com>
2023-08-14 08:56:53 +0530

    #ms 3project initial commit

---
+++ springboot/MovieCatalogueService/src/test/java/com/practice/simple/MovieCatalogueService/MovieCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.simple.MovieCatalogueService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springboot/MovieInformationService/src/test/java/com/practice/simple/MovieInformationService/MovieInformationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.simple.MovieInformationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
