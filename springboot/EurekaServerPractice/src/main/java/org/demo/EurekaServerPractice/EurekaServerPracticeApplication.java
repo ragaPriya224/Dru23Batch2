27f688da87f8ac5115c555a909b1a24fecc7b5de
priya <priya@zawqwe.com>
2023-08-16 11:55:53 +0530

    sd - add annotation

--- springboot/EurekaServerPractice/src/main/java/org/demo/EurekaServerPractice/EurekaServerPracticeApplication.java
+++ springboot/EurekaServerPractice/src/main/java/org/demo/EurekaServerPractice/EurekaServerPracticeApplication.java
@@ -2,8 +2,10 @@ package org.demo.EurekaServerPractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerPracticeApplication {
 
 	public static void main(String[] args) {
