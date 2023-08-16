41f700fe49e6cdf86847f1847109ab37fb1c51b9
priya <priya@zawqwe.com>
2023-08-16 13:08:34 +0530

    sd eureka client

--- springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
+++ springboot/MovieCatalogueService/src/main/java/com/practice/simple/MovieCatalogueService/MovieCatalogController.java
@@ -22,11 +22,11 @@ public class MovieCatalogController {
 		//1 -> get all rated movie id's
 //		List<Rating> ratings = Arrays.asList(new Rating("222",4),
 //				new Rating("41",5));
-		UserRating ur = rt.getForObject("http://localhost:8083/users/"+userId, UserRating.class);
+		UserRating ur = rt.getForObject("http:/rating-service/users/"+userId, UserRating.class);
 		
 		
 		return ur.getUserRating().stream().map(rating ->{
-			Movie movie =rt.getForObject("http://localhost:8082/movie/"+rating.getMoveiId(), Movie.class);
+			Movie movie =rt.getForObject("http://movie-info-service/movie/"+rating.getMoveiId(), Movie.class);
 			return new CatalogItem(movie.getName(),"test",rating.getRating());
 		}).collect(Collectors.toList());
 	}
