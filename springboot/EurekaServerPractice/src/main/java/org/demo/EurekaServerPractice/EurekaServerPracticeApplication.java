a2497d452490b122092a631d4a9ee4625ab2d63c
priya <priya@zawqwe.com>
2023-08-16 11:51:43 +0530

    eureka server initial commit

---
+++ springboot/EurekaServerPractice/src/main/java/org/demo/EurekaServerPractice/EurekaServerPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.demo.EurekaServerPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerPracticeApplication.class, args);
+	}
+
+}
